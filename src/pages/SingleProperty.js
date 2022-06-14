4e6edcdcf2533c4b955b3b68b6cb6d320537eced
m-marc <marchenkovmaxim@gmail.com>
2022-06-14 20:11:32 +0200

    single property component

--- src/pages/SingleProperty.js
+++ src/pages/SingleProperty.js
@@ -1,10 +1,18 @@
 import {useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {fetchSingleProperty} from "../contentful";
 
 const SingleProperty = () => {
 	const {id} = useParams()
+	// const [property, setProperty] = useState({})
 
 	const property = useSelector(state => state.properties.properties.find(prop => prop.id === id))
+
+	useEffect(() => {
+		// fetchSingleProperty(id)
+		// 	.then(({property}) => setProperty(property))
+	}, [])
 
 	if (!property)
 		return (<section>
